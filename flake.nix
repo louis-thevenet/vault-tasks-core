fa7a3540d67287beb82f3f4ed4fd646a6d53aa11
Louis Thevenet <louis.tvnt@gmail.com>
2024-11-17 10:19:42 +0100

    chore: add dev env

---
+++ flake.nix
@@ -0,0 +1,74 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+
+    # Dev tools
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        let
+          cargoToml = builtins.fromTOML (builtins.readFile ./vault-tasks/Cargo.toml);
+          rust-toolchain = pkgs.symlinkJoin {
+            name = "rust-toolchain";
+            paths = with pkgs; [
+              rustc
+              cargo
+              cargo-watch
+              rust-analyzer
+              rustPlatform.rustcSrc
+              cargo-dist
+              cargo-tarpaulin
+              cargo-insta
+              cargo-machete
+              cargo-edit
+              clippy
+            ];
+          };
+
+          buildInputs = with pkgs; [ ];
+          nativeBuildInputs = with pkgs; [ ];
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [
+              config.treefmt.build.devShell
+            ];
+            RUST_BACKTRACE = "full";
+            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+
+            nativeBuildInputs = nativeBuildInputs;
+            buildInputs = buildInputs ++ [
+              rust-toolchain
+            ];
+          };
+
+          # Add your auto-formatters here.
+          # cf. https://numtide.github.io/treefmt/
+          treefmt.config = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixpkgs-fmt.enable = true;
+              nixfmt.enable = true;
+            };
+          };
+        };
+    };
+}
